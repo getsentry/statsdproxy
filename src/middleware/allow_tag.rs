3d76605acaa3ad2d2fb92eb1b8b1e1b8005afb21
Matt Cannizzaro <matthew.cannizzaro@sentry.io>
2023-08-22 13:01:43 -0700

    Move tag deny list to proper middleware

--- src/middleware/allow_tag.rs
+++ src/middleware/allow_tag.rs
@@ -25,33 +25,7 @@ impl Middleware for AllowTag {
     }
 
     fn submit(&mut self, metric: Metric) -> Result<(), Overloaded> {
-        let mut tags_to_keep = Vec::new();
-        let mut rewrite_tags = false;
-
-        for tag in metric.tags_iter() {
-            if tag.name().is_some_and(|t| self.tags.contains(t)) {
-                rewrite_tags = true;
-            } else {
-                tags_to_keep.push(tag);
-            }
-        }
-
-        if rewrite_tags {
-            let mut rewriten_metric = metric.clone();
-            let tag_bytes = tags_to_keep.iter().map(|t| t.raw);
-            
-            let mut tag_buffer = Vec::new();
-            for t in tag_bytes {
-                tag_buffer.extend(t);
-                
-                tag_buffer.push(b',');
-            }
-            rewriten_metric.set_tags(&tag_buffer[0..tag_buffer.len() - 1]); // omit trailing ',' from loop above
-
-            self.next.submit(rewriten_metric)
-        } else {
-            self.next.submit(metric)
-        }
+        self.next.submit(metric)
     }
 
     fn join(&mut self) -> Result<(), Error> {
