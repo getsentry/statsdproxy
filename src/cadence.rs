07e9513296c18af622371e1d92dc0efe2e99b362
Matt Cannizzaro <matthew.cannizzaro@sentry.io>
2023-08-23 13:49:20 -0700

    Add Cadence metric sink

---
+++ src/cadence.rs
@@ -0,0 +1,77 @@
+use std::{sync::Mutex, io};
+
+use cadence::MetricSink;
+
+use crate::{middleware::Middleware, types::Metric};
+
+pub struct StatsdProxyMetricSink<M> {
+    next: Mutex<M>,
+}
+
+impl<M> StatsdProxyMetricSink<M>
+where
+    M: Middleware
+{
+    pub fn new(next: M) -> StatsdProxyMetricSink<M> {
+        StatsdProxyMetricSink { next: Mutex::new(next) }
+    }
+}
+
+impl<M> MetricSink for StatsdProxyMetricSink<M>
+where
+    M: Middleware
+{    
+    // FIXME: There's a bit of an impedance mismatch between Cadence's metric sinks and our middleware interface,
+    // so this is not entirely correct:
+    //
+    // 1) The return value from `emit` on success is supposed to be the number of bytes writen to the sink,
+    // or zero if the write was buffered. `Middleware` doesn't have a way to propagate this information up
+    // from one middleware to the next.
+    //
+    // 2) `flush` is supposed to force a flush of all buffered metrics, but there's no way to ask the
+    // next middleware to do this.
+    
+    fn emit(&self, raw_metric: &str) -> io::Result<usize> {
+        let cooked_metric = Metric::new(raw_metric.as_bytes().to_vec());
+        let mut next = self.next.lock().unwrap();
+        let result = next.submit(cooked_metric);
+
+        match result {
+            Ok(_) => Ok(raw_metric.len()),
+            Err(_) => Err(io::Error::new(io::ErrorKind::Other, "error writing metric")),
+        }
+    }
+
+    fn flush(&self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+    use std::sync::RwLock;
+
+    use super::*;
+    use cadence::prelude::*;
+    use cadence::StatsdClient;
+    use crate::testutils::FnStep;
+
+    #[test]
+    fn basic() {
+        let results = Arc::new(RwLock::new(vec![]));
+        let results2 = results.clone();
+        let next = FnStep(move |metric| {
+            results.write().unwrap().push(metric);
+            Ok(())
+        });
+
+        let sink = StatsdProxyMetricSink::new(next);
+        let client = StatsdClient::from_sink("test.metrics", sink);
+
+        client.incr("test.counter").unwrap();
+        client.gauge("test.gauge", 42).unwrap();
+
+        assert_eq!(results2.read().unwrap().len(), 2);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod middleware;
+pub mod config;
+#[cfg(feature = "cadence")]
+pub mod cadence;
+
+mod testutils;
+mod types;
